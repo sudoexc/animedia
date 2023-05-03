0a3e474deb5f4957eac51272221990e438b0f155
joe <javokhir.369@gmail.com>
2023-05-03 19:00:26 +0500

    3 May

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use stricts';
 
-const NumberOfAnime = +prompt('Сколько аниме вы посмотрели?', '99');
+const NumberOfAnime = +prompt('Сколько аниме вы посмотрели?', '');
 
 const personalAnimeDB = {
 	count: NumberOfAnime,
@@ -10,11 +10,27 @@ const personalAnimeDB = {
 	privat: false
 };
 
-const a = prompt('Один из последних просмотренних фильмов?', 'Onepiece'),
-	  b = +prompt('Насколько его оцените?', ''),
-	  d = prompt('Один из последних просмотренних фильмов?', 'Onepiece'),
-	  e = +prompt('Насколько его оцените?', '');
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренних фильмов?', ''),
+	 	  b = +prompt('Насколько его оцените?', '');
 
-personalAnimeDB.movies[a] = b;
+	if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalAnimeDB.movies[a] = b;
+	} else {
+		i--;
+	}
+
+	if(personalAnimeDB.count < 10) {
+		console.log('просмотренно довольно мало аниме')
+	} else if(personalAnimeDB.count > 10 && personalAnimeDB.count < 30) {
+		console.log('вы классический зритель')
+	} else if(personalAnimeDB.count > 30) {
+		console.log('вы киноман')
+	} else {
+		console.log('ошибка')
+	}
+
+}
+
 
 console.log(personalAnimeDB);
